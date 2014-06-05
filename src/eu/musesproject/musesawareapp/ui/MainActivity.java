1560ccae80e82df9a2104ef9fe309358084981a2
yasali <yasir.ali@swedenconnectivity.com>
2014-06-05 15:30:36 +0200

    button changed with correct properties.

--- src/eu/musesproject/musesawareapp/ui/MainActivity.java
+++ src/eu/musesproject/musesawareapp/ui/MainActivity.java
@@ -2,11 +2,11 @@ package eu.musesproject.musesawareapp.ui;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -16,7 +16,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import eu.musesproject.client.contextmonitoring.service.aidl.Action;
 import eu.musesproject.musesawareapp.R;
 import eu.musesproject.musesawareapp.sensorserviceconsumer.MusesServiceConsumer;
@@ -25,12 +24,11 @@ import eu.musesproject.musesawareapp.sensorserviceconsumer.ServiceModel;
 public class MainActivity extends Activity implements View.OnClickListener{
 
 	private static String TAG = MainActivity.class.getSimpleName();
-	private Button open, run,install,access; 
+	private Button open,openConfAssetButton, run,install,access; 
 	private TextView resultView;
 	// Muses Service
 	private ServiceModel serviceModel;
 	private MusesServiceConsumer musesService;
-	private Context context;
 	// messages from service consumer
 	public static final int ACTION_ACCEPTED = 1;
 	public static final int ACTION_DENIED = 0;
@@ -40,16 +38,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		open = (Button) findViewById(R.id.open_btn);
+		openConfAssetButton = (Button) findViewById(R.id.open_conf_btn);
 		run = (Button) findViewById(R.id.run_btn);
 		install = (Button) findViewById(R.id.install_btn);
 		access = (Button) findViewById(R.id.access_btn);
-
+		
 		resultView = (TextView) findViewById(R.id.result_text_view);
+
 		open.setOnClickListener(this);
+		openConfAssetButton.setOnClickListener(this);
 		run.setOnClickListener(this);
 		install.setOnClickListener(this);
 		access.setOnClickListener(this);
-		context = getApplicationContext();
 		regiterForMusesService();
 		
 	}
@@ -65,18 +65,47 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.open_btn :
-			if (isServiceRunning()) {
-				sendUserActionsToRemoteMusesService(new Action("open", System.currentTimeMillis()));
-			}
+			Map<String, String> openAssetProperties = new HashMap<String, String>();
+			openAssetProperties.put("resourceName","statistics");
+			openAssetProperties.put("resourceType","insensitive");
+			openAssetProperties.put("resourcePath","/sdcard/Swe/MUSES_beer_competition.txt");
+			Action openAssetAction = new Action("open_asset", System.currentTimeMillis());
+			sendUserActionsToRemoteMusesService(openAssetAction, openAssetProperties);
+			break;
+		case R.id.open_conf_btn :
+			Map<String, String> openConfAssetProperties = new HashMap<String, String>();
+			openConfAssetProperties.put("resourceName","statistics");
+			openConfAssetProperties.put("resourceType","insensitive");
+			openConfAssetProperties.put("resourcePath","/sdcard/Swe/MUSES_partner_grades.txt");
+			Action openConfAssetAction = new Action("open_asset", System.currentTimeMillis());
+			sendUserActionsToRemoteMusesService(openConfAssetAction, openConfAssetProperties);
 			break;
 		case R.id.run_btn :
-			sendUserActionsToRemoteMusesService(new Action("run", System.currentTimeMillis()));
+			Map<String, String> runProperties = new HashMap<String, String>();
+			runProperties.put("protocol","https");
+			runProperties.put("url","https://...");
+			runProperties.put("resourceId","file1.png");
+			runProperties.put("method","post");
+			Action runAction = new Action("run", System.currentTimeMillis());
+			sendUserActionsToRemoteMusesService(runAction,runProperties);
 			break;
 		case R.id.install_btn :
-			sendUserActionsToRemoteMusesService(new Action("install", System.currentTimeMillis()));
+			Map<String, String> installProperties = new HashMap<String, String>();
+			installProperties.put("protocol","https");
+			installProperties.put("url","https://...");
+			installProperties.put("resourceId","file1.png");
+			installProperties.put("method","post");
+			Action installAction = new Action("install", System.currentTimeMillis());
+			sendUserActionsToRemoteMusesService(installAction,installProperties);
 			break;
 		case R.id.access_btn :
-			sendUserActionsToRemoteMusesService(new Action("access", System.currentTimeMillis()));
+			Map<String, String> accessProperties = new HashMap<String, String>();
+			accessProperties.put("protocol","https");
+			accessProperties.put("url","https://...");
+			accessProperties.put("resourceId","file1.png");
+			accessProperties.put("method","post");
+			Action accessAction = new Action("access", System.currentTimeMillis());
+			sendUserActionsToRemoteMusesService(accessAction,accessProperties);
 			break;
 		}
 	}
@@ -103,13 +132,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		musesService.startService();
 	}
 	
-	private void sendUserActionsToRemoteMusesService(Action action){
-		Map<String, String> properties = new HashMap<String, String>();
-		properties.put("protocol","https");
-		properties.put("url","https://...");
-	    properties.put("resourceId","file1.png");
-	    properties.put("method","post");
-	    	       
+	private void sendUserActionsToRemoteMusesService(Action action, Map<String, String> properties){
 		if (serviceModel.getService().musesServiceConsumer != null) {
 			try {
 				Log.d(TAG, "Sending user's action");
