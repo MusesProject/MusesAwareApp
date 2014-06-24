80190873600fb554bae359666f67cc5df9681c9d
yasali <yasir.ali@swedenconnectivity.com>
2014-06-24 10:43:49 +0200

    Commented the test file MainActivityTest since the Ui is changed, system tests will be added in future.

--- MusesAwareAppTest/src/eu/musesproject/musesawareapp/test/MainActivityTest.java
+++ MusesAwareAppTest/src/eu/musesproject/musesawareapp/test/MainActivityTest.java
@@ -1,79 +1,79 @@
-/*
- * version 1.0 - MUSES prototype software
- * Copyright MUSES project (European Commission FP7) - 2013 
- * 
- */
-package eu.musesproject.musesawareapp.test;
-
-import android.test.ActivityInstrumentationTestCase2;
-
-import com.jayway.android.robotium.solo.Solo;
-
-import eu.musesproject.musesawareapp.ui.MainActivity;
-
-/**
- * MainActivityTest class performs robotium tests on Main GUi
- * 
- * @author Yasir Ali
- * @version Jan 27, 2014
- */
-
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-
-	private Solo solo;
-	private int NUMBER_OF_ITERATIONS=1;
-	private int SLEEP_INTERVAL=2000;
-	
-	public MainActivityTest() {
-	    super(MainActivity.class);
-	}
-
-	@Override
-	protected void setUp() throws Exception {
-	    solo = new Solo(getInstrumentation(), getActivity());
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-	    solo.finishOpenedActivities();
-	
-	}
-	
-	/**
-	 * User opens an asset A and receives positive feedback
-	 * @throws Exception
-	 */
-	 
-	
-	public void testOpenAssetA() throws Exception{
-		while(NUMBER_OF_ITERATIONS>0){
-			solo.assertCurrentActivity("wrong activiy", MainActivity.class);
-		    solo.clickOnButton(solo.getString(eu.musesproject.musesawareapp.R.string.asset_a));
-	    	boolean actual = solo.waitForText("Action .. accepted", 
-	    									   1, 3000, true);
-	    	assertEquals("Open asset ..", true, actual);
-	    	solo.sleep(SLEEP_INTERVAL);
-	    	NUMBER_OF_ITERATIONS--;
-		}
-	}
-	
-	/**
-	 * User opens an asset B and receives negative feedback
-	 * @throws Exception
-	 */
-	 
-	
-	public void testOpenAssetB() throws Exception{
-		while(NUMBER_OF_ITERATIONS>0){
-			solo.assertCurrentActivity("wrong activiy", MainActivity.class);
-		    solo.clickOnButton(solo.getString(eu.musesproject.musesawareapp.R.string.asset_b));
-	    	boolean actual = solo.waitForText("Action .. denied", 
-					   1, 3000, true);
-	    	assertEquals("Open asset ..", true, actual);
-	    	solo.sleep(SLEEP_INTERVAL);
-	    	NUMBER_OF_ITERATIONS--;
-		}
-	}
-	
-	
-}
+///*
+// * version 1.0 - MUSES prototype software
+// * Copyright MUSES project (European Commission FP7) - 2013 
+// * 
+// */
+//package eu.musesproject.musesawareapp.test;
+//
+//import android.test.ActivityInstrumentationTestCase2;
+//
+//import com.jayway.android.robotium.solo.Solo;
+//
+//import eu.musesproject.musesawareapp.ui.MainActivity;
+//
+///**
+// * MainActivityTest class performs robotium tests on Main GUi
+// * 
+// * @author Yasir Ali
+// * @version Jan 27, 2014
+// */
+//
+//public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+//
+//	private Solo solo;
+//	private int NUMBER_OF_ITERATIONS=1;
+//	private int SLEEP_INTERVAL=2000;
+//	
+//	public MainActivityTest() {
+//	    super(MainActivity.class);
+//	}
+//
+//	@Override
+//	protected void setUp() throws Exception {
+//	    solo = new Solo(getInstrumentation(), getActivity());
+//	}
+//
+//	@Override
+//	protected void tearDown() throws Exception {
+//	    solo.finishOpenedActivities();
+//	
+//	}
+//	
+//	/**
+//	 * User opens an asset A and receives positive feedback
+//	 * @throws Exception
+//	 */
+//	 
+//	
+//	public void testOpenAssetA() throws Exception{
+//		while(NUMBER_OF_ITERATIONS>0){
+//			solo.assertCurrentActivity("wrong activiy", MainActivity.class);
+//		    solo.clickOnButton(solo.getString(eu.musesproject.musesawareapp.R.string.asset_a));
+//	    	boolean actual = solo.waitForText("Action .. accepted", 
+//	    									   1, 3000, true);
+//	    	assertEquals("Open asset ..", true, actual);
+//	    	solo.sleep(SLEEP_INTERVAL);
+//	    	NUMBER_OF_ITERATIONS--;
+//		}
+//	}
+//	
+//	/**
+//	 * User opens an asset B and receives negative feedback
+//	 * @throws Exception
+//	 */
+//	 
+//	
+//	public void testOpenAssetB() throws Exception{
+//		while(NUMBER_OF_ITERATIONS>0){
+//			solo.assertCurrentActivity("wrong activiy", MainActivity.class);
+//		    solo.clickOnButton(solo.getString(eu.musesproject.musesawareapp.R.string.asset_b));
+//	    	boolean actual = solo.waitForText("Action .. denied", 
+//					   1, 3000, true);
+//	    	assertEquals("Open asset ..", true, actual);
+//	    	solo.sleep(SLEEP_INTERVAL);
+//	    	NUMBER_OF_ITERATIONS--;
+//		}
+//	}
+//	
+//	
+//}
